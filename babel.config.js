137883009c8c221c5212864f0e31c4394365428c
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-09-17 21:23:48 -0500

    initial commit

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    // ! https://stackoverflow.com/questions/33117227/setting-environment-variable-in-react-native
+    plugins: ["transform-inline-environment-variables"],
   };
 };
