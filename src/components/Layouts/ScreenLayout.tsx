760810a6c985daa5e554a3beaa246a198a48825e
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-11-23 06:39:42 -0600

    feat: updates ScreenLayout styles

--- src/components/Layouts/ScreenLayout.tsx
+++ src/components/Layouts/ScreenLayout.tsx
@@ -1,4 +1,4 @@
-import { ComponentProps, FC, ReactNode, useState } from "react";
+import { ComponentProps, FC, ReactNode } from "react";
 import { SafeAreaView, Text, View } from "react-native";
 import IconInput from "@/src/components/Inputs/IconInput";
 import { createStyleSheet, useStyles } from "react-native-unistyles";
@@ -55,37 +55,34 @@ const ScreenLayout: FC<ScreenLayoutProps> = ({
   );
 };
 
-const stylesheet = createStyleSheet(
-  ({ colors, font, spacing, rounded, name }) => ({
-    container: {
-      backgroundColor: name === "dark" ? colors.black : colors.gray[50],
-    },
-    headerContainer: {
-      borderWidth: 1,
-      paddingHorizontal: 36,
-      paddingVertical: 24,
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-    },
-    screenHeading: {
-      color: name === "dark" ? colors.white : colors.gray[800],
-      fontSize: font.size["2xl"],
-      fontWeight: "600",
-    },
-    headerIcon: {
-      color: name === "dark" ? colors.white : colors.gray[800],
-    },
-    searchInputContainer: {
-      borderColor: name === "dark" ? colors.purple[400] : colors.gray[300],
-      borderBottomWidth: 1,
-      paddingHorizontal: 36,
-      paddingVertical: 16,
-    },
-    searchInputIcon: {
-      color: colors.gray[500],
-    },
-  })
-);
+const stylesheet = createStyleSheet(({ colors, font, spacing, name }) => ({
+  container: {
+    backgroundColor: name === "dark" ? colors.black : colors.gray[50],
+  },
+  headerContainer: {
+    paddingHorizontal: spacing[9],
+    paddingTop: spacing[6],
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  screenHeading: {
+    color: name === "dark" ? colors.white : colors.gray[800],
+    fontSize: font.size["2xl"],
+    fontWeight: "600",
+  },
+  headerIcon: {
+    color: name === "dark" ? colors.white : colors.gray[800],
+  },
+  searchInputContainer: {
+    borderColor: name === "dark" ? colors.purple[600] : colors.purple[400],
+    borderBottomWidth: spacing["0.5"],
+    paddingHorizontal: spacing[9],
+    paddingVertical: spacing[4],
+  },
+  searchInputIcon: {
+    color: colors.gray[500],
+  },
+}));
 
 export default ScreenLayout;
