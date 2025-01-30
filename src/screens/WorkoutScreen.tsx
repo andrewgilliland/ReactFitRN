1e6b961b8e5b18a98ec890eea487fcd3014a4c67
Andrew Gilliland <andrewpgilliland@gmail.com>
2025-01-29 20:59:31 -0600

    feat: updates ui for WorkoutScreen

--- src/screens/WorkoutScreen.tsx
+++ src/screens/WorkoutScreen.tsx
@@ -1,19 +1,27 @@
 import { useEffect, useState } from "react";
-import { SafeAreaView, View } from "react-native";
+import {
+  Dimensions,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  View,
+} from "react-native";
 import { router, useLocalSearchParams } from "expo-router";
 import { createStyleSheet, useStyles } from "react-native-unistyles";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import { getExerciseById, getWorkoutById } from "@/src/lib/supabase";
 import { Button, Text } from "@/components";
-import { spacing } from "../styles";
+import { colors, rounded, spacing } from "../styles";
 import { Workout, workout } from "../types/Workout";
 import Entypo from "@expo/vector-icons/Entypo";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function WorkoutScreen() {
   const [workout, setWorkout] = useState<Workout>({
     id: 1,
     title: "Chest Day",
-    description: "A workout focused on chest muscles",
+    description:
+      "A workout focused on chest muscles and strength. This workout is designed to help you build strength and muscle mass in your chest. It includes a variety of exercises that target different parts of the chest, including the upper, middle, and lower chest.",
     type: "strength",
     equipment: ["barbell", "dumbbell", "bodyweight"],
     exercises: [1, 2, 3],
@@ -24,6 +32,13 @@ export default function WorkoutScreen() {
     },
   });
   const { id } = useLocalSearchParams<{ id: string }>();
+  const screenHeight = Dimensions.get("window").height;
+  const headerHeight = screenHeight * 0.3; // 60% of screen height
+
+  const setCount = Object.values(workout.sets).reduce(
+    (acc, sets) => acc + sets.length,
+    0
+  );
 
   useEffect(() => {
     // (async () => {
@@ -38,28 +53,113 @@ export default function WorkoutScreen() {
   }, []);
 
   const {
-    styles: { container },
+    styles: { container, contentContainer, pill },
   } = useStyles(stylesheet);
 
   return (
-    <SafeAreaView>
-      <View style={container}>
-        <Button size="square" theme="neutral" onPress={() => router.back()}>
-          <Entypo name="chevron-left" size={24} color="white" />
-        </Button>
+    <View>
+      <Button
+        size="square"
+        theme="neutral"
+        onPress={() => router.back()}
+        style={{ position: "absolute", zIndex: 1, top: 40, left: 20 }}
+      >
+        <Entypo name="chevron-left" size={24} color="white" />
+      </Button>
+      <ScrollView>
+        <View
+          style={[
+            { height: headerHeight, backgroundColor: colors.orange[700] },
+            container,
+          ]}
+        ></View>
 
-        <View>
+        <View
+          style={[
+            container,
+            contentContainer,
+            { height: screenHeight - headerHeight },
+          ]}
+        >
+          <View style={[pill, { marginTop: spacing[4] }]}>
+            <Text
+              color="orange.500"
+              size="sm"
+              weight="bold"
+              style={{ textTransform: "capitalize" }}
+            >
+              {workout.type}
+            </Text>
+          </View>
           <Text
             weight="bold"
             color="neutral.100"
-            size="2xl"
+            size="3xl"
             style={{ marginTop: spacing[6], textTransform: "capitalize" }}
           >
             {workout.title}
           </Text>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: spacing[4],
+              marginTop: spacing[4],
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: spacing[1],
+              }}
+            >
+              <MaterialCommunityIcons
+                name="dumbbell"
+                size={18}
+                color={colors.orange[600]}
+              />
+              <Text
+                color="neutral.400"
+                weight="semibold"
+              >{`${setCount.toString()} sets`}</Text>
+            </View>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: spacing[1],
+              }}
+            >
+              <AntDesign
+                name="clockcircle"
+                size={18}
+                color={colors.blue[700]}
+              />
+              <Text color="neutral.400" weight="semibold">{`30min`}</Text>
+            </View>
+          </View>
+          <View
+            style={{
+              marginTop: spacing[6],
+              alignSelf: "flex-start",
+            }}
+          >
+            <Text weight="semibold">Overview</Text>
+            <Text
+              color="neutral.300"
+              weight="regular"
+              style={{ marginTop: spacing[4] }}
+            >
+              {workout.description}
+            </Text>
+          </View>
         </View>
-      </View>
-    </SafeAreaView>
+      </ScrollView>
+    </View>
   );
 }
 
@@ -68,6 +168,14 @@ const stylesheet = createStyleSheet(({ colors, fontSize, spacing }) => ({
     paddingHorizontal: spacing[9],
     paddingVertical: spacing[6],
   },
+  contentContainer: {
+    borderRadius: rounded.xl,
+    backgroundColor: colors.neutral[950],
+    marginTop: -spacing[6],
+    flexDirection: "column",
+
+    alignItems: "center",
+  },
   headerContainer: {
     borderWidth: 1,
     paddingHorizontal: spacing[9],
@@ -75,5 +183,11 @@ const stylesheet = createStyleSheet(({ colors, fontSize, spacing }) => ({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
+  },
+  pill: {
+    backgroundColor: colors.orange[950],
+    borderRadius: rounded.xl,
+    paddingHorizontal: spacing[4],
+    paddingVertical: spacing[2],
   },
 }));
